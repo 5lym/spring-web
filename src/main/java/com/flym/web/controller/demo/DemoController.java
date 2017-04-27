aa8a987da04604e2d91c092fc93e8576be13c967
Carlis Liu <carlisliu@live.com>
2017-04-27 15:29:10 +0800

    rename

---
+++ src/main/java/com/flym/web/controller/demo/DemoController.java
@@ -0,0 +1,7 @@
+package com.flym.web.controller.demo;
+
+/**
+ * Created by Administrator on 4/27/2017.
+ */
+public class DemoController {
+}
---
+++ src/main/java/com/flym/web/controller/demo/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 4/27/2017.
+ */
+package com.flym.web.controller.demo;
